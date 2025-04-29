79ec1d1c05f26cabc9f63b30074257220d9e2eda
divyanshistomar <divyanshisinghtomar05@gmail.com>
2025-04-29 12:49:27 +0530

    feature-frontpage added

---
+++ script.js
@@ -0,0 +1,19 @@
+const music = new Audio('vande.mp3');
+
+// create Array 
+
+const songs = [
+    {
+        id:'1',
+        songName:`Aaoge jab tum  <br>
+        <div class="subtitle">Jab we met</div>`,
+        poster: "tumsehi.jpg"
+    },
+    {
+        id:'2',
+        songName:`Tere Bina <br>
+        <div class="subtitle">Guru</div>`,
+        poster: "terebina.jpg"
+    },
+    
+
